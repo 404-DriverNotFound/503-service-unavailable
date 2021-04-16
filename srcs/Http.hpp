e343b8d55864c7ec1d179e2498f4df6867fb18e9
minckim <minckim@student.42seoul.kr>
2021-04-16 15:09:37 +0900

    Fix : Http
    manager : minckim
    
    http 작업중중

--- srcs/Http.hpp
+++ srcs/Http.hpp
@@ -42,28 +42,42 @@
 // 	std::string		get_protocol() const;
 // };
 
-enum						e_message_type
-{
-	REQ,
-	RES,
-};
+// enum						e_message_type
+// {
+// 	REQ,
+// 	RES,
+// };
 
-class StartLine
-{
-	public:
-	e_message_type				message_type;
-	std::vector<std::string>	tokens;
-								StartLine();
-								StartLine(e_message_type t, std::string& str);
-								StartLine(const StartLine& x);
-	StartLine&					operator=(const StartLine& x);
-	virtual						~StartLine();
-	std::string					get_protocol() const;
-};
+// class StartLine
+// {
+// 	public:
+// 	e_message_type				message_type;
+// 	std::vector<std::string>	tokens;
+// 								StartLine();
+// 								StartLine(e_message_type t, std::string& str);
+// 								StartLine(const StartLine& x);
+// 	StartLine&					operator=(const StartLine& x);
+// 	virtual						~StartLine();
+// 	std::string					get_protocol() const;
+// };
 
 class Http
 {
-	StartLine									start_line;
-	std::multimap<std::string, std::string>		headers;
-	std::deque<uint8_t>							body;
+	std::multimap<std::string, std::string>	headers;
+	std::deque<uint8_t>						body;
+	std::string								protocol;
+	int										version_major;
+	int										version_minor;
+};
+
+class HttpReq : public Http
+{
+	e_method								method;
+	std::string								path;
+};
+
+class HttpRes : public Http
+{
+	int										status_code;
+	std::map<int, std::string>*				map_code;
 };
