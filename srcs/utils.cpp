5c0a6bd1ed0d219042ceea3f46f5c61aec778d98
jack-r-lantern <tjdfkr2421@gmail.com>
2021-04-16 15:07:26 +0900

    FIX: function
    manager: sujung
    
    get_set_token
    get_chr_token

--- srcs/utils.cpp
+++ srcs/utils.cpp
@@ -44,23 +44,24 @@ bool	get_seq_token(std::string &origin, std::string::iterator &it, std::string &
 	return false;
 }
 
-bool	get_set_token(std::string &origin, std::string::iterator &it, std::string &token, char *set, size_t	reserve_size = 1000)
+int	get_set_token(std::string &origin, std::string::iterator &it, std::string &token, char *set, size_t	reserve_size = 1000)
 {
+	char	*p;
 	std::string::iterator	end = origin.end();
 
 	token.clear();
 	token.reserve(reserve_size);
 	while (it != end)
 	{
-		if (strchr(set, *it))
+		if ((p = strchr(set, *it)))
 		{
 			it += 1;
-			return true;
+			return *p;
 		}
 		token.push_back(*it);
 		++it;
 	}
-	return false;
+	return 0;
 }
 
 bool	get_chr_token(std::string &origin, std::string::iterator &it, std::string &token, char c, size_t reserve_size = 1000)
@@ -74,6 +75,8 @@ bool	get_chr_token(std::string &origin, std::string::iterator &it, std::string &
 		if (*it == c)
 		{
 			it += 1;
+			while (it != end && *it == c)
+				++it;
 			return true;
 		}
 		token.push_back(*it);
