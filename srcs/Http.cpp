b88d2e9eae113e80fd459f71a9542cb2d66c27fb
minckim <minckim@student.42seoul.kr>
2021-04-16 16:09:38 +0900

    ETC: 집에 가자
    manager : minckim

--- srcs/Http.cpp
+++ srcs/Http.cpp
@@ -1,1 +1,11 @@
 #include "Http.hpp"
+
+void		HttpReq::set_start_line(const std::string& line)
+{
+	std::string					token;
+	std::string::const_iterator	it;
+
+	get_chr_token(line, it, token, ' ');
+	token.
+
+}
--- srcs/Http.hpp
+++ srcs/Http.hpp
@@ -7,61 +7,7 @@
 #include "Path.hpp"
 #include "Utils.hpp"
 
-// class StartLine
-// {
-// 	public:
-// 	std::string		protocol;
-// 					StartLine();
-// 					StartLine(const std::string& str);
-// 					StartLine(const StartLine& x);
-// 	StartLine&		operator=(const StartLine& x);
-// 	virtual			~StartLine();
-// 	std::string		get_protocol() const;
-// };
-
-// class StartLineReq : public StartLine
-// {
-// 	public:
-// 	e_method		method;
-// 	Path			path;
-// 					StartLineReq();
-// 					StartLineReq(const StartLineReq& x);
-// 					StartLineReq(const std::string& str);
-// 	virtual			~StartLineReq();
-// 	std::string		get_protocol() const;
-// };
-
-// class StartLineRes : public StartLine
-// {
-// 	public:
-// 	uint8_t			code;
-// 					StartLineRes();
-// 					StartLineRes(const StartLineRes& x);
-// 					StartLineRes(const std::string& str);
-// 	virtual			~StartLineRes();
-// 	std::string		get_protocol() const;
-// };
-
-// enum						e_message_type
-// {
-// 	REQ,
-// 	RES,
-// };
-
-// class StartLine
-// {
-// 	public:
-// 	e_message_type				message_type;
-// 	std::vector<std::string>	tokens;
-// 								StartLine();
-// 								StartLine(e_message_type t, std::string& str);
-// 								StartLine(const StartLine& x);
-// 	StartLine&					operator=(const StartLine& x);
-// 	virtual						~StartLine();
-// 	std::string					get_protocol() const;
-// };
-
-class Http
+struct Http
 {
 	std::multimap<std::string, std::string>	headers;
 	std::deque<uint8_t>						body;
@@ -70,14 +16,16 @@ class Http
 	int										version_minor;
 };
 
-class HttpReq : public Http
+struct HttpReq : public Http
 {
 	e_method								method;
 	std::string								path;
+
+	void									set_start_line(const std::string& line);
 };
 
-class HttpRes : public Http
+struct HttpRes : public Http
 {
 	int										status_code;
-	std::map<int, std::string>*				map_code;
+	static std::map<int, std::string>		map_code;
 };
