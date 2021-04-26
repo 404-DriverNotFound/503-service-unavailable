c23cfa459fc8474cbabb520198d17ba996f8aaaa
minckim <minckim@student.42seoul.kr>
2021-04-26 15:12:30 +0900

    ADD: Stream

--- includes/Stream.hpp
+++ includes/Stream.hpp
@@ -1,7 +1,8 @@
-#include <vector>
+#include <list>
 #include "Utils.hpp"
 
-using std::vector;
+using std::list;
+using std::string;
 
 struct Buffer
 {
@@ -11,10 +12,17 @@ struct Buffer
 
 struct Stream
 {
-	vector<Buffer>	buffers;
+	int						fd_in;		// 클라이언트 소켓
+	list<Buffer>			buffers;
+	list<Buffer>::iterator	it_list;
+	uint8_t*				it_buffer;
 
-	ssize_t			fill_buffer(int fd);						// 채운 바이트 수, 음수는 오류
-	ssize_t			fill_buffer(uint8_t* start, uint8_t* end);	// 채운 바이트 수, 음수는 오류
-	ssize_t			read(uint8_t* bf, size_t s);				// 읽은 바이트 수, 음수는 오류
-	ssize_t			write(uint8_t* bf, size_t s);				// 쓴 바이트 수, 음수는 오류
+	ssize_t			read(uint8_t* b, size_t s);				// 스트림 내부 버퍼로부터 밖으로 읽기
+	ssize_t			write(int fd, uint8_t* b, size_t s);	// 스트림 내부 버퍼로부터 외부의 fd로 바로 쓰기
+	void			fill(size_t s);							// 클라이언트 소켓으로부터 새로운 버퍼 채우기
+	bool			get_seq_token(string &token, const char *seq, size_t reserve_size = 1000);
+	int				get_set_token(string &token, const char *set, size_t reserve_size = 1000);
+	bool			get_chr_token(string &token, const char c, size_t reserve_size = 1000);
+	void			get_set_token(list<string>& tokens, const char* set);
+	void			get_chr_token(list<string>& tokens, const char* set);
 };
