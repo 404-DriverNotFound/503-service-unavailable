8006722cdfd640ff5aba07d183ace3a32f724937
minckim <minckim@student.42seoul.kr>
2021-05-07 20:47:36 +0900

    POINT: TEST OK
    할일:
    파일디스크립터 누수 체크
    이너 클래스 정상적인 소멸 확인
    메이크파일에 플래그 넣어서 컴파일
    디버깅 출력 코드 지우고 속도 측정

--- class_sujung/utils.hpp
+++
@@ -1,7 +0,0 @@
-#include <string>
-#include <unistd.h>
-#include <iostream>
-
-bool	get_seq_token(std::string &origin, std::string::iterator &it, std::string &token, char *seq, size_t	reserve_size = 1000);
-int		get_set_token(std::string &origin, std::string::iterator &it, std::string &token, char *set, size_t	reserve_size = 1000);
-bool	get_chr_token(std::string &origin, std::string::iterator &it, std::string &token, char c, size_t reserve_size = 1000);
